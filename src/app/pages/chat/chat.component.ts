fc66f99f328d91454a49d994f5a7e066a3ac9b7e
ArturMiguel <arturmrocha1@gmail.com>
2024-03-16 15:16:01 -0300

    feat: Criando input de envio de texto

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import SweetAlert from 'src/app/libs/SweetAlert';
+import { OpenaiService } from 'src/app/services/openai.service';
 
 @Component({
   selector: 'app-chat',
@@ -8,30 +10,25 @@ import { Component, OnInit } from '@angular/core';
 export class ChatComponent implements OnInit {
   public messageList = [
     {
-      "message": "Que tipo de doença pode ser blablabla",
-      "type": "USER",
-      "thread": "thread_Tzr0tZtYhXUge2GL5PkPrKgE"
-    },
-    {
-      "message": "Ah, isso é uma traça-do-tomateiro! Também conhecida como Tuta absoluta, essa pequena mariposa é uma praga bem chata para os tomateiros. Elas colocam seus ovos nas folhas, e quando as larvas nascem, começam a se alimentar das folhas, caules e frutos, o que pode causar sérios danos às plantas. É importante ficar de olho e tomar medidas para controlar essas pragas, como usar armadilhas, inseticidas naturais ou até mesmo inseticidas químicos, se necessário.",
+      "message": "Olá, bem-vindo(a) ao IA.AGRO. Em que posso ajudar?",
       "type": "BOT",
-      "thread": "thread_Tzr0tZtYhXUge2GL5PkPrKgE"
-    },
-    {
-      "message": "E como combato ela?",
-      "type": "USER",
-      "thread": "thread_Tzr0tZtYhXUge2GL5PkPrKgE"
-    },
-    {
-      "message": "Existem várias formas de combater a traça-do-tomateiro, algumas mais naturais e outras mais químicas. Aqui estão algumas opções:",
-      "type": "BOT",
-      "thread": "thread_Tzr0tZtYhXUge2GL5PkPrKgE"
+      "thread": ""
     },
   ]
+  public message = "";
 
-  constructor() { }
+  constructor(
+    private openService: OpenaiService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  sendMessage() {
+    this.openService.sendMessage(this.message).then((response) => {
+      console.log(response);
+    }).catch((error: any) => {
+      SweetAlert.error("", error.error.message);
+    })
+  }
 }
