42c035709fe7e5e57598c0d897d368668599764f
Diego Dias Pereira <diegodiasptc@gmail.com>
2024-03-16 14:04:24 -0300

    feat: adicona a pagina de camera

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CameraComponent } from './pages/camera/camera.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'camera',
+    component: CameraComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CameraComponent } from './pages/camera/camera.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    CameraComponent
   ],
   imports: [
     BrowserModule,
