c70b526eac02fb909a6619880a4bfc3bd233eaa2
rafael.romancini <rafael.romancini@tecprime.com.br>
2024-03-16 16:25:05 -0300

    Finalizada tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CameraComponent } from './pages/camera/camera.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {
     path: 'camera',
     component: CameraComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   }
 ];
 
